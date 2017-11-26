895ab63af34b8c618c41a869f274f22d0b8b62ee
Nantia Manoli <33526180+NantiaManoli@users.noreply.github.com>
2017-11-26 16:53:20 +0200

    Make icons bigger on player
    Client_last/tv/songs

--- project/client_last/tv/songs/js/player.js
+++ project/client_last/tv/songs/js/player.js
@@ -4,21 +4,21 @@ function setPlayer() {
     `<div class="player text-center">
         <div class="col-md-3 hidden-xs hidden-sm" style="padding-right:0px">
             <div class="col-xs-3" style="padding:10px 0px 10px 0px;">
-                <img class="img" style="width:80px" src="../ressrc/icon.png">
+                <img class="img" style="width:105px" src="../ressrc/icon.png">
             </div>
-            <div class="col-xs-9 text-left">
-				<p class="title">Read All About It <button class="clickableElement"><em style="font-size:24px" class="fa">&#xf067;</em></button></p>
+            <div style="font-size: 25px;" class="col-xs-9 text-left">
+				<p class="title">Read All About It <button style="font-size: 50px; margin-left: 20px; padding: 0;" class="clickableElement">+</button></p>
 				<p class="artist">Emeli Sandé</p>
             </div>
         </div>
     
         <div class="col-xs-12 col-sm-12 col-md-7 col-lg-6" style="padding:0">
             <div class="controls">
-                <button class="clickableElement" onclick="playerAPI.shuffle()"><em style="font-size:24px" class="fa">&#xf074;</em></button>
-                <button class="clickableElement" onClick="playerAPI.prev()"><em style="font-size:24px" class="fa">&#xf048;</em></button>
-                <button class="clickableElement" onclick="playerAPI.playPause()"><em style="font-size:35px" class="fa">&#xf01d;</em></button>
-                <button class="clickableElement" onClick="playerAPI.next()"><em style="font-size:24px" class="fa">&#xf051;</em></button>
-                <button class="clickableElement" onClick="playerAPI.repeat()"><em style="font-size:24px; color:#9999a5" class="fa">&#xf01e;</em></button>
+                <button class="clickableElement" onclick="playerAPI.shuffle()"><em style="font-size:40px" class="fa">&#xf074;</em></button>
+                <button class="clickableElement" onClick="playerAPI.prev()"><em style="font-size:40px" class="fa">&#xf048;</em></button>
+                <button class="clickableElement" onclick="playerAPI.playPause()"><em style="font-size:65px" class="fa">&#xf01d;</em></button>
+                <button class="clickableElement" onClick="playerAPI.next()"><em style="font-size:40px" class="fa">&#xf051;</em></button>
+                <button class="clickableElement" onClick="playerAPI.repeat()"><em style="font-size:40px; color:#9999a5" class="fa">&#xf01e;</em></button>
             </div>
         
             <div>
@@ -32,9 +32,8 @@ function setPlayer() {
         </div>
         
         <div class="col-xs-3 col-sm-2 col-lg-3" style="padding-top:30px">
-            <button class="clickableElement"><em style="font-size:24px" class="fa">&#xf0ca;</em></button>
-            <button class="expand clickableElement"><em style="font-size:24px" class="fa">&#xf065;</em></button>
-            <button class="clickableElement muted"><em style="font-size:24px" class="fa">&#xf027;</em></button>
+            <button class="expand clickableElement"><em style="font-size:40px" class="fa">&#xf065;</em></button>
+            <button class="clickableElement muted"><em style="font-size:40px" class="fa">&#xf027;</em></button>
             <div style="margin-bottom:4px" class="volume">	<span class="volumeBar"></span></div>
         </div>
     
