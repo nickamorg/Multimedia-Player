063d5de36328e2839f2e8af97ee2e33631b67ca0
Nikolaos Simidalas <nickamorg0@gmail.com>
2017-12-01 22:19:05 +0200

    TV - Scroll with kinect

--- project/client_last/tv/songs/js/playerAPI.js
+++ project/client_last/tv/songs/js/playerAPI.js
@@ -949,6 +949,54 @@ playerAPI.songs = {
     });
 
     $("#playing")[0].load();
+
+    function scrolling() {
+        let bottom_counter = 0;
+        let bottom = 100;
+        let top_counter = 0;
+        let top = 150;
+        setInterval(function() {
+            if($("#cursor").offset().top < 60) {
+                bottom_counter = 0;
+                bottom = 100;
+                top_counter++;
+                if(top_counter === 5) {
+                    top_counter = 0;
+                    if($(".pt-page-current")[0].id.includes("movie")) {
+                        $(".pt-page-current").find(".movies_content").scrollTop(top);
+                    } else if($(".pt-page-current")[0].id.includes("serie")) {
+                        $(".pt-page-current").find(".series_content").scrollTop(top);
+                    } else if($(".pt-page-current")[0].id.includes("song") === true) {
+                        $(".pt-page-current").find(".content").scrollTop(top);
+                    }
+                    top = top - 100;
+                }
+
+            } else if($("#cursor").offset().top > 620) {
+                top_counter = 0;
+                top = 100;
+                bottom_counter++;
+                if(bottom_counter === 5) {
+                    bottom_counter = 0;
+                    if($(".pt-page-current")[0].id.includes("movie")) {
+                        $(".pt-page-current").find(".movies_content").scrollTop(bottom);
+                    } else if($(".pt-page-current")[0].id.includes("serie")) {
+                        $(".pt-page-current").find(".series_content").scrollTop(bottom);
+                    } else if($(".pt-page-current")[0].id.includes("song") === true) {
+                        $(".pt-page-current").find(".content").scrollTop(bottom);
+                    }
+                    bottom = bottom + 100;
+                }
+            } else {
+                bottom_counter = 0;
+                bottom = 100;
+                top_counter = 0;
+                top = 100;
+            }
+        }, 1000);
+    }
+
+    scrolling();
 })();
 
 playerAPI.playPause = function playPause() {
