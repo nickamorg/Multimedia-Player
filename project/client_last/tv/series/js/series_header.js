79b994b97bf73b110247b5d9c90ad52644c64f11
Nantia Manoli <33526180+NantiaManoli@users.noreply.github.com>
2017-12-11 15:34:04 +0200

    TV Songs, Series Back button

--- project/client_last/tv/series/js/series_header.js
+++ project/client_last/tv/series/js/series_header.js
@@ -3,8 +3,8 @@ function setSeriesHeader(active) {
 
         `<div>
         <ul style="margin: 0; position:relative">
-            <li style="float:left"><a onclick="visitedPagesStack.goToLastVisitedPage()" style="padding-right:0"><em class="fa fa-arrow-left" aria-hidden="true" style="padding-top:9px;"></em></a></li>
-            <li style="float:left"><a class="to_lobby">3Player</a></li>
+            <li class="clickableElement" style="float:left"><a onclick="visitedPagesStack.goToLastVisitedPage()" style="padding-right: 26px;"><em class="fa fa-arrow-left" aria-hidden="true" style="padding-top:9px;"></em></a></li>
+            <li class="clickableElement" style="float:left"><a class="to_lobby">3Player</a></li>
             <li style="float: left">
                 <form style="padding-top:10px" class="navbar-form navbar-left">
                     <div class="input-group">
--- project/client_last/tv/songs/js/header.js
+++ project/client_last/tv/songs/js/header.js
@@ -2,8 +2,8 @@ function setHeader(active) {
     document.write(
         `<div>
         <ul style="margin: 0; position:relative">
-            <li style="float:left" class="clickableElement"><a onclick="visitedPagesStack.goToLastVisitedPage()" style="padding-right:0"><em class="fa fa-arrow-left" aria-hidden="true" style="padding-top:9px;"></em></a></li>
-            <li style="float:left"><a class="to_lobby clickableElement">3Player</a></li>
+            <li class="clickableElement" style="float:left" class="clickableElement"><a onclick="visitedPagesStack.goToLastVisitedPage()" style="padding-right: 26px;"><em class="fa fa-arrow-left" aria-hidden="true" style="padding-top:9px;"></em></a></li>
+            <li class="clickableElement" style="float:left"><a class="to_lobby clickableElement">3Player</a></li>
             <li style="float: left">
                 <form style="padding-top:10px" class="navbar-form navbar-left">
                     <div class="input-group">
